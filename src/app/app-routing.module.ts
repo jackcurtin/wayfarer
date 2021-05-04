48367717b228bc246d65778b1070b0007bda7f78
Jack Curtin <jackcurtin94@gmail.com>
2021-05-04 11:37:04 -0500

    set up router between cities and landing page, created home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CitiesComponent} from './cities/cities.component';
+import {BrowserModule} from '@angular/platform-browser';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'cities',
+    component: CitiesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
