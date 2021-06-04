80deefba121d0c035e038a248a1fb421a84e8921
Jack Curtin <jackcurtin94@gmail.com>
2021-06-04 17:09:02 -0500

    debug heroku

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/wayfarer'));
+app.use(express.static(__dirname + '/dist/mock-wayfarer'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/wayfarer/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/mock-wayfarer/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
